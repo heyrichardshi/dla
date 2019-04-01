eaf3776b7351c2653f501c90574d9c1ef3d5bdfd
Richard Shi <heyrichardshi@users.noreply.github.com>
2019-04-01 11:41:11 -0400

    Improved runtime
    Split aggregate into four quadrants

--- dla.cpp
+++ dla.cpp
@@ -11,8 +11,14 @@ using namespace std;
 
 extern const double PI;
 
-DLA::DLA() : seedRadius(1), killRadius(3), stickDistance(0.25), stepLength(0.5), maxSize(0), size(0) {
+DLA::DLA() : seedRadius(1), killRadius(3), stickDistance(0.25), stepLength(0.5), maxSize(0), size(0), furthestDistance(0) {
     push(0, 0);
+    pushQ1(0, 0);
+    pushQ2(0, 0);
+    pushQ3(0, 0);
+    pushQ4(0, 0);
+    furthest[0] = 0;
+    furthest[1] = 0;
 }
 
 bool DLA::walk() {
@@ -41,21 +47,155 @@ void DLA::push(double x, double y) {
     size++;
 }
 
-bool DLA::stick(double x, double y) {
-    double distance = sqrt(pow(x,2) + pow(y,2));
+void DLA::pushQ1(double x, double y) {
+    double origin[2] = {x, y};
+    vector<double> v(origin, origin + sizeof(origin) / sizeof(double));
+    NE.push_back(v);
+}
 
-    if (distance > maxSize + stickDistance) {
+void DLA::pushQ2(double x, double y) {
+    double origin[2] = {x, y};
+    vector<double> v(origin, origin + sizeof(origin) / sizeof(double));
+    NW.push_back(v);
+}
+
+void DLA::pushQ3(double x, double y) {
+    double origin[2] = {x, y};
+    vector<double> v(origin, origin + sizeof(origin) / sizeof(double));
+    SW.push_back(v);
+}
+
+void DLA::pushQ4(double x, double y) {
+    double origin[2] = {x, y};
+    vector<double> v(origin, origin + sizeof(origin) / sizeof(double));
+    SE.push_back(v);
+}
+
+int DLA::quadrant(double theta) { // theta within [-PI, PI]
+    if ((theta >= 0) && (theta < PI / 2)) {
+        return 1; // quadrant 1 (NE)
+    }
+    else if ((theta >= PI / 2) && (theta < PI)) {
+        return 2; // quadrant 2 (NW)
+    }
+    else if ((theta >= -PI / 2) && (theta < 0)) {
+        return 4; // quadrant 4 (SE)
+    }
+    else {
+        return 3; // quadrant 3 (SW)
+    }
+}
+
+bool DLA::stick(double x, double y) {
+    double radius = sqrt(pow(x,2) + pow(y,2));
+
+    if (radius > furthestDistance + stickDistance) {
         return false;
     }
 
-    for (vector<vector<double>>::reverse_iterator i = aggregate.rbegin(); i != aggregate.rend(); i++) {
-        distance = sqrt(pow(x - (*i)[0], 2) + pow(y - (*i)[1], 2));
+    double theta = atan2(y, x);
+
+    switch(quadrant(theta)) {
+        case 1:
+            for (vector<vector<double>>::reverse_iterator i = NE.rbegin(); i != NE.rend(); i++ ) {
+        double distance = sqrt(pow(x - (*i)[0], 2) + pow(y - (*i)[1], 2));
         if (distance < stickDistance) {
             push(x, y);
+            pushQ1(x, y);
+            if (abs(x) <= stickDistance) {
+                pushQ2(x, y);
+            }
+            if (abs(y) <= stickDistance) {
+                pushQ4(x, y);
+            }
+
+            if (radius > furthestDistance) {
+                furthest[0] = x;
+                furthest[1] = y;
+                furthestDistance = radius;
+            }
+
             calcMaxSize();
-            cout << size << ": " << x << " " << y << endl; // TODO: cout
+            cout << size << endl; // TODO: cout
             return true;
         }
+    }
+            break;
+        case 2:
+            for (vector<vector<double>>::reverse_iterator i = NW.rbegin(); i != NW.rend(); i++ ) {
+        double distance = sqrt(pow(x - (*i)[0], 2) + pow(y - (*i)[1], 2));
+        if (distance < stickDistance) {
+            push(x, y);
+            pushQ2(x, y);
+            if (abs(x) <= stickDistance) {
+                pushQ1(x, y);
+            }
+            if (abs(y) <= stickDistance) {
+                pushQ3(x, y);
+            }
+
+            if (radius > furthestDistance) {
+                furthest[0] = x;
+                furthest[1] = y;
+                furthestDistance = radius;
+            }
+
+            calcMaxSize();
+            cout << size << endl; // TODO: cout
+            return true;
+        }
+    }
+            break;
+        case 3:
+            for (vector<vector<double>>::reverse_iterator i = SW.rbegin(); i != SW.rend(); i++ ) {
+        double distance = sqrt(pow(x - (*i)[0], 2) + pow(y - (*i)[1], 2));
+        if (distance < stickDistance) {
+            push(x, y);
+            pushQ3(x, y);
+            if (abs(x) <= stickDistance) {
+                pushQ4(x, y);
+            }
+            if (abs(y) <= stickDistance) {
+                pushQ2(x, y);
+            }
+
+            if (radius > furthestDistance) {
+                furthest[0] = x;
+                furthest[1] = y;
+                furthestDistance = radius;
+            }
+
+            calcMaxSize();
+            cout << size << endl; // TODO: cout
+            return true;
+        }
+    }
+            break;
+        case 4:
+            for (vector<vector<double>>::reverse_iterator i = SE.rbegin(); i != SE.rend(); i++ ) {
+        double distance = sqrt(pow(x - (*i)[0], 2) + pow(y - (*i)[1], 2));
+        if (distance < stickDistance) {
+            push(x, y);
+            pushQ4(x, y);
+            if (abs(x) <= stickDistance) {
+                pushQ3(x, y);
+            }
+            if (abs(y) <= stickDistance) {
+                pushQ1(x, y);
+            }
+
+            if (radius > furthestDistance) {
+                furthest[0] = x;
+                furthest[1] = y;
+                furthestDistance = radius;
+            }
+
+            calcMaxSize();
+            cout << size << endl; // TODO: cout
+            return true;
+        }
+    }
+            break;
     }
 
     return false;
@@ -75,8 +215,9 @@ void DLA::calcMaxSize() {
         return;
     }
 
-    for (auto i : aggregate) {
-        double distance = sqrt(pow(x - i[0], 2) + pow(y - i[1], 2));
+
+    for (vector<vector<double>>::reverse_iterator i = aggregate.rbegin(); i != aggregate.rend(); i++ ) {
+        double distance = sqrt(pow(x - (*i)[0], 2) + pow(y - (*i)[1], 2));
         maxSize = distance > maxSize ? distance : maxSize;
     }
 
@@ -84,10 +225,6 @@ void DLA::calcMaxSize() {
         seedRadius = 2 * maxSize;
         killRadius = 5 * maxSize;
     }
-}
-
-int DLA::getSize() {
-    return size;
 }
 
 void DLA::run(int N) {
--- dla.hpp
+++ dla.hpp
@@ -20,6 +20,12 @@ class DLA {
     double stickDistance;
     double maxSize;
     vector<vector<double>> aggregate;
+    double furthest[2];
+    double furthestDistance;
+    vector<vector<double>> NE;
+    vector<vector<double>> NW;
+    vector<vector<double>> SW;
+    vector<vector<double>> SE;
     int size;
 public:
     DLA();
@@ -27,8 +33,12 @@ public:
     bool stick(double x, double y);
     bool kill(double x, double y);
     void calcMaxSize();
+    int quadrant(double theta);
     void push(double x, double y);
-    int getSize();
+    void pushQ1(double x, double y);
+    void pushQ2(double x, double y);
+    void pushQ3(double x, double y);
+    void pushQ4(double x, double y);
     void run(int N);
     void print(string filename);
 };
